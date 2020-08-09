cdeac636ba878dd8527beb74e8be7a8dfe80c60b
vanesing <vanesing@mail.ru>
2020-08-09 23:33:50 +0300

    Было интересно. Особым наслаждением было к JDK12 FX прикручивать, форумы спасли))) незабываемо в общем))) 1.8 JDK билд 260... что то там хоть и скачаный с oracle сайта что-то у меня не завелся и вообще Fatal Error JVM аж выдал.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,37 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class Controller {
+
+    //public void btnOneClickAction (ActionEvent actionEvent){
+      //  System.out.println("Hello");
+    // }
+
+    @FXML
+    TextArea mainTextArea;
+    @FXML
+    Button Send;
+    @FXML
+    TextField mainTextField;
+
+
+    public void btnOneClickAction(ActionEvent actionEvent) {
+
+        String message = "";
+         //mainTextField.set(mainTextArea);
+
+
+      mainTextArea.appendText(mainTextField.getText());
+
+
+    }
+
+
+
+
 }
